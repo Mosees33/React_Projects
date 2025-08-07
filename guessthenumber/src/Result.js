639bd1d7f05acc140908c7012a32b623f6a16a66
Mosees33 <moseesmohan@gmail.com>
2025-08-07 22:32:18 +0530

    newProject : guess_the_number_game project

---
+++ guessthenumber/src/Result.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Result({secretNumber,Term}){
+    let result;
+
+    if(Term){
+        if(Term > secretNumber){
+        result = "Higher";
+    }else if(Term < secretNumber){
+        result = "Lower";
+    }else if(Term == secretNumber){
+        result = "Yipee! correct"
+    }else{
+        result = "Enter valid Input";
+    }
+    }
+
+    return <h3>You guessed : {result}</h3>
+}
+
+export default Result;
