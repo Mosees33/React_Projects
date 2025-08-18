8f807d9e1057a349ed0e805777d565210304343d
Mosees33 <moseesmohan@gmail.com>
2025-08-18 09:50:57 +0530

    new: new project

--- mini-ecommerce/frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return (
-        <footer className="py-1 bg-dark">
-            <p className="text-center text-white mt-1 ">
-                Mosees - 2024-2025, All Rights Reserved
-            </p>
-        </footer> 
-    )
-}
